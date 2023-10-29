6b3d056d86e04824e3fbf23f56ff01908395ab78
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-10-29 16:19:00 +0100

    Update SprayControl.py

--- SprayControl.py
+++ SprayControl.py
@@ -2,6 +2,9 @@ import keyboard
 import pyautogui
 
 while True:
+    #            EDIT 'a' to your macro key for activate it.
     if keyboard.is_pressed('a'):
+    #            EDIT 'a' to your macro key for activate it.
+        
         current_x, current_y = pyautogui.position()
         pyautogui.moveTo(current_x, current_y + 6, duration=0.25)
